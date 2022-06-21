5057d4fd650ead302ce772e449d9adeb14fad810
Vinicius Lima <viniciuslimavirtuel@gmail.com>
2022-06-21 17:16:20 -0300

    build 2

--- script.js
+++ script.js
@@ -14,12 +14,17 @@ let hr = 0
 function start() {
     startTimer = setInterval(function () {
         ms++
+        if(ms === 100) {
+            seg++
+            ms = 0
+        }
         atualizaValor()
     }, 10);
 }
 
 function atualizaValor() {
     th.innerHTML = ms
+    seconds.innerHTML = seg
 }
 
 btnStart.addEventListener("click", start)
